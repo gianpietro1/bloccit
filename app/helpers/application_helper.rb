ec9bce8231b4041f95c7ecadf24eccb8d0a36f6b
Gianpietro Lavado <gianpietro1@gmail.com>
2015-01-21 13:15:41 -0500

    Modified form_group_tag helper with alternative

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
     'gianpietro1'
   end
 
-  def form_group_tag(errors, &block)
+  def form_group_class(errors)
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
+      "form-group has-error"
     else
-      content_tag :div, capture(&block), class: 'form-group'
+      "form-group"
     end
   end
 
