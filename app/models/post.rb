dc97615b75f7a659c16ba05ad460e6c18302cf6b
Gianpietro Lavado <gianpietro1@gmail.com>
2015-02-07 00:45:49 -0500

    added more rspecs

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,8 +21,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
@@ -30,10 +30,6 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank, new_rank)
   end
-
-  after_create :create_vote
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
