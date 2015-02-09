c858dfa4ec7e949625d7743b484f4d53f7e8342c
Gianpietro Lavado <gianpietro1@gmail.com>
2015-02-09 13:00:04 -0500

    refactoring home page, arrows, permissions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,9 +10,15 @@ class Comment < ActiveRecord::Base
   private
 
   def send_favorite_emails
-    post.favorites.each do |favorites|
-      FavoriteMailer.new_comment(favorite.user, post, self).deliver
+    post.favorites.each do |favorite|
+      if should_receive_update_for?(favorite)
+        FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+      end
     end
+  end
+
+  def should_receive_update_for?(favorite)
+    user_id != favorite.user_id && favorite.user.email_favorites?
   end
 
 end
