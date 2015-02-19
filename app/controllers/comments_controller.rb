e5e280be912143863ffc5ad27712bf90a16a9b75
Gianpietro Lavado <gianpietro1@gmail.com>
2015-02-18 21:49:56 -0500

    Complete creating with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,15 +11,21 @@ class CommentsController < ApplicationController
  def create
     @post = Post.find(params[:post_id])
     @topic = @post.topic
+    
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    @new_comment = Comment.new
+
     if @comment.save
       flash[:notice] = "Comment was saved"
-      redirect_to [@topic,@post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic,@post]
     end
+
+    respond_with(@comment) do |format|
+    format.html {redirect_to [@post.topic, @post] }
+
+  end
  end
 
 def destroy
